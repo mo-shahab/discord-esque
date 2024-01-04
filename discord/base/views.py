fd56af45a5b2f3de1191aabd5d96449cf6e0432c
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-01-04 12:13:55 +0530

    added the directory of templates

--- discord/base/views.py
+++ discord/base/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'a good chatroom'},
+    {'id': 2, 'name': 'a really good chatroom'},
+    {'id': 3, 'name': 'those arent good chatrooms, you know this'}
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
-    return HttpResponse('Room page')
+    return render(request, 'room.html')
