f2b644e208d2ad392d64fbe8b4412a6d015c0164
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-01-10 20:45:52 +0530

    database related configurations

--- discord/discord/settings.py
+++ discord/discord/settings.py
@@ -76,12 +76,16 @@ WSGI_APPLICATION = "discord.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "djongo",
+#         "NAME": "chatrooms",
+#         'ENFORCE_SCHEMA': False,
+#         'CLIENT': {
+#                 'host': "mongodb://localhost: 27017/"
+#             }  
+#     }
+# }
 
 
 # Password validation
