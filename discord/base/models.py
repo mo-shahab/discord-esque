13981efac32e5f2e014cbd51446980ba8c3f58ba
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-01-12 00:49:37 +0530

    db configs

--- discord/base/models.py
+++ discord/base/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Person(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants = 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+    
+    
--- discord/base/views.py
+++ discord/base/views.py
@@ -1,29 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client['chatrooms']
-collection_name = db['rooms']
-
-# Fetch data from MongoDB
-rooms = list(collection_name.find({}, {'_id': 0}))
-
-# Close the MongoDB connection
-client.close()
 
 # Create your views here.
 def home(request):
-    # rooms = list(collection_name.find())
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for r in rooms:
-        if r['id'] == int(pk):
-            room = r
+    room = Room.objects.get(id=pk)
     context = {'room': room}
-            
     return render(request, 'base/room.html', context)
