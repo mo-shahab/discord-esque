0a9a81eb1098aad94f4f5705ac19097cd360cacf
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-01-16 01:08:15 +0530

    deleted database.py

--- discord/database.py
+++ discord/database.py
@@ -1,17 +1,4 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client['chatrooms']
-collection_name = db['rooms']
-
-rooms_data = [
-    {'id': 1, 'name': 'something just like that'},
-    {'id': 2, 'name': 'a really good chatroom'},
-    {'id': 3, 'name': 'those arent good chatrooms, you know this'}
-]
-
-# Inserting data into MongoDB
-collection_name.insert_many(rooms_data)
-
-# Close the MongoDB connection
-client.close() 
+i = 0
+while i < 19:
+    print('f')
+    i += 1
