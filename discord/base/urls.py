03e1134d4738b49dca6bc5d02420a8844445d661
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-01-16 01:07:04 +0530

    delete-messages routing

--- discord/base/urls.py
+++ discord/base/urls.py
@@ -3,10 +3,14 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
-    path('logout', views.logoutUser, name="logout"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
+
     path('', views.home, name="home"),
     path("room/<str:pk>/", views.room, name="room"),
+
     path("create-room/", views.createRoom, name="create-room"),
     path("update-room/<str:pk>/", views.updateRoom, name="update-room"),
     path("delete-room/<str:pk>", views.deleteRoom, name="delete-room"),
+    path("delete-messages/<str:pk>", views.deleteMessages, name="delete-messages"),
 ]
